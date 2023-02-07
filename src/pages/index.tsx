0f0c9b0da877445e18b42f32d85c33ae849e7b39
fhero2030 <jcarloasilo@gmail.com>
2023-02-07 16:14:42 +0800

    login page design implementation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,19 +2,73 @@ import type { GetServerSideProps, NextPage } from "next";
 
 import { getSession, signIn } from "next-auth/react";
 
-import { Stack, Button } from "@mui/material";
+import { Button, Typography, Grid, Box, CssBaseline, Avatar, Paper } from "@mui/material";
+
+import GoogleIcon from "@mui/icons-material/Google";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+
+function Copyright(props: any) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {"Copyright © PITO"} {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
 const Home: NextPage = () => {
   return (
-    <Stack>
-      <Button
-        onClick={() => {
-          signIn("google");
+    <Grid container component="main" sx={{ height: "100vh" }}>
+      <CssBaseline />
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage: "url(https://source.unsplash.com/ssKEI4HRRtI)",
+          backgroundRepeat: "no-repeat",
+          backgroundColor: (t) =>
+            t.palette.mode === "light" ? t.palette.grey[50] : t.palette.grey[900],
+          backgroundSize: "cover",
+          backgroundPosition: "center",
         }}
-      >
-        Sign in
-      </Button>
-    </Stack>
+      />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100%",
+            pb: 20,
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Welcome to ICT INVENTORY
+          </Typography>
+          <Box sx={{ mt: 1 }}>
+            <Button
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              endIcon={<GoogleIcon />}
+              onClick={() => {
+                signIn("google");
+              }}
+            >
+              Sign In with Google
+            </Button>
+
+            <Copyright sx={{ mt: "auto" }} />
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
   );
 };
 
